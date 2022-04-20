2d9e87c5ec9ee25d7c6c18375841f39a5f65cea7
Wael <waelmohamedhassn.acc@gmail.com>
2022-04-20 02:04:21 +0200

    todo ghfghfghfghfghfghfghfgh

--- lib/firebase_utils.dart
+++ lib/firebase_utils.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:todo_app/model/task.dart';
 CollectionReference<Task> getTasksCollection(){
   var typedCollection = FirebaseFirestore.instance
